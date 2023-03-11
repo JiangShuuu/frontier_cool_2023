6d8772c3fb7e5885e4359b8425b13fb4cb86f949
JiangShuuu <globelex65@gmail.com>
2023-03-11 11:22:47 +0800

    add eslint prettier

---
+++ 01/.eslintrc.cjs
@@ -0,0 +1,34 @@
+module.exports = {
+	root: true,
+	env: {
+		browser: true,
+		node: true,
+	},
+	extends: [
+		'eslint-config-prettier',
+		'eslint:recommended',
+		'plugin:@typescript-eslint/recommended',
+		'plugin:vue/vue3-recommended',
+		'plugin:vue/vue3-essential',
+		'plugin:prettier/recommended',
+	],
+	parser: 'vue-eslint-parser',
+	parserOptions: {
+		ecmaVersion: 2020,
+		sourceType: 'module',
+		parser: '@typescript-eslint/parser',
+	},
+	plugins: [],
+	globals: {
+		defineProps: 'readonly',
+		defineEmits: 'readonly',
+		defineExpose: 'readonly',
+		withDefaults: 'readonly',
+	},
+	rules: {
+		'no-console': process.env.NODE_ENV === 'production' ? ['error', { allow: ['error', 'warn'] }] : 'off',
+		'no-debugger': process.env.NODE_ENV === 'production' ? 'warn' : 'off', 
+		'@typescript-eslint/no-unused-vars': ['error', { varsIgnorePattern: '.*', args: 'none' }], 
+		'@typescript-eslint/no-explicit-any': 'off', 
+	},
+};
