3790776db34efb439b0a728d0b9cec10a3dd238f
JiangShuuu <globelex65@gmail.com>
2023-03-11 13:00:24 +0800

    add cards

---
+++ 01/src/components/Card.vue
@@ -0,0 +1,5 @@
+<template>
+	<div>Card</div>
+</template>
+
+<script lang="ts" setup></script>
--- 01/src/views/Home.vue
+++ 01/src/views/Home.vue
@@ -1,8 +1,21 @@
 <template>
 	<!-- Header -->
 	<Header />
+	<div class="grid grid-cols-5 gap-4 pt-5">
+		<Card />
+		<Card />
+		<Card />
+		<Card />
+		<Card />
+		<Card />
+		<Card />
+		<Card />
+		<Card />
+		<Card />
+	</div>
 </template>
 
 <script lang="ts" setup>
 import Header from '../components/Global/Header.vue';
+import Card from '../components/Card.vue';
 </script>
