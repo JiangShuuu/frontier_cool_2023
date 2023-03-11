edfa61fc5e937797b7d27cba187cf247c08606c0
JiangShuuu <globelex65@gmail.com>
2023-03-11 14:55:09 +0800

    add path file

--- 01/src/views/Home.vue
+++ 01/src/views/Home.vue
@@ -5,7 +5,7 @@
 </template>
 
 <script lang="ts" setup>
-import Header from '../components/Header.vue';
-import Container from '../components/Container/index.vue';
-import Pagination from '../components/Pagination.vue';
+import Header from '~/components/Header.vue';
+import Container from '~/components/Container/index.vue';
+import Pagination from '~/components/Pagination.vue';
 </script>
