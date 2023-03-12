cff9f0709c776f001570d6705adc61355e5a3626
JiangShuuu <globelex65@gmail.com>
2023-03-12 11:45:46 +0800

    upd modal

--- 01/src/components/Modal.vue
+++ 01/src/components/Modal.vue
@@ -1,17 +1,18 @@
 <template lang="pug">
 div(
-  v-if="modalData"
+  v-if="modalData",
+  :class="[ modalIsOpen ? 'opacity-100' : 'opacity-0 pointer-events-none' ]"
   class="fixed top-0 left-0 w-screen h-screen p-5 flex justify-center items-center transition-all duration-300"
-  :class=[modalIsOpen ? 'opacity-100' : 'opacity-0 pointer-events-none']
 )
+
   div(
     class="absolute top-0 left-0 w-full h-full bg-black/50",
     @click="$emit('modal-change')"
   )
 
   div(
+    :class="[ modalIsOpen ? 'scale-100' : 'scale-0' ]"
     class="w-full max-w-sm bg-white rounded-md overflow-hidden z-10 transition-all duration-300"
-    :class="[modalIsOpen ? 'scale-100' : 'scale-0' ]"
   )
     div(
       class="border-b-2 p-3 flex justify-between items-center"
@@ -44,9 +45,7 @@ div(
         div(
           class="p-3 w-full space-y-4"
         )
-          div(
-            class="flex items-center justify-center"
-          )
+          div(class="flex items-center justify-center")
             img(
               v-if="!error",
               :src="modalData.avatar",
@@ -56,27 +55,19 @@ div(
               @error="handleError"
             )
 
-            div(
-              v-if="loading && !error",
-              class="w-48 h-48 flex items-center justify-center"
-            )
+            div(v-if="loading && !error", class="w-48 h-48 flex items-center justify-center")
               Icon(icon="uiw:loading", class="w-8 h-8 animate-spin")
 
-            div(
-              v-if="error",
-              class="w-48 h-48 flex items-center justify-center"
-            )
+            div(v-if="error", class="w-48 h-48 flex items-center justify-center")
               Icon(icon="material-symbols:error-outline-rounded", class="w-8 h-8")
 
-          div(
-            class="space-y-1"
-          )
-            p {{ 'name: ' + modalData.name }}
-            p {{ 'age: ' + modalData.age }}
-            p {{ 'gender: ' + modalData.gender }}
-            p {{ 'birthday: ' + modalData.birthday }}
-            p {{ 'phone: ' + modalData.phone }}
-            p {{ 'email: ' + modalData.email }}
+          div(class="space-y-1")
+            p {{ `name: ${modalData.name}` }}
+            p {{ `age: ${modalData.age}` }}
+            p {{ `gender: ${modalData.gender}` }}
+            p {{ `birthday: ${modalData.birthday}` }}
+            p {{ `phone: ${modalData.phone}` }}
+            p {{ `email: ${modalData.email}` }}
 </template>
 
 <script lang="ts" setup>
--- 01/src/views/Home.vue
+++ 01/src/views/Home.vue
@@ -11,6 +11,7 @@ Error(v-if="isError")
 Container(v-if="!isLoading && data", :render-modal="renderModal", :data="data", :current-page="currentPage", :select-sort="selectSort", @get-total-page="getTotalPage")
 Pagination(:total="totalPage", :current-page="currentPage", @set-current-page="setCurrentPage")
 Modal(:modal-is-open="modalIsOpen", :modal-data="modalData", @modal-change="modal.handleClick")
+	
 </template>
 
 <script lang="ts" setup>
@@ -66,8 +67,8 @@ watch(selectSort, (val, oldVal) => {
 // Modal
 const modalIsOpen = ref(false);
 const modalData = ref();
-const modal: Toggle = {
-	handleClick: (value: TypeDummyData) => {
+const modal: any = {
+	handleClick: (value: any) => {
 		if (value) {
 			modalData.value = value;
 			modalIsOpen.value = true;
