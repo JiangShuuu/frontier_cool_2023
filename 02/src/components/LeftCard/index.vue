b51fc59c2b29599ebc1225cf30cc45e9bba69c14
JiangShuuu <globelex65@gmail.com>
2023-03-12 21:19:17 +0800

    first modal

--- 02/src/components/LeftCard/index.vue
+++ 02/src/components/LeftCard/index.vue
@@ -1,11 +1,63 @@
 <template>
-  <div class="border border-blue">
-    <div class="flex items-center justify-end p-3">
+  <div class="border border-blue text-white">
+    <!-- <div class="flex items-center justify-end p-3">
       <button class="bg-white px-2 py-2.5 font-bold">+ Add New Pair</button>
     </div>
-    <Card />
+    <Card /> -->
+    <div>
+    <div v-for="(item, index) in input.items" :key="index">
+      <label>
+        Key:
+        <input type="text" v-model="item.key" @input="generateObject()">
+      </label>
+      <label>
+        Value:
+        <input type="text" v-model="item.value" @input="generateObject()">
+      </label>
+    </div>
+    <button @click="addItem">Add Item</button>
+    <div>
+      <pre>{{ input.output }}</pre>
+    </div>
+  </div>
   </div>
 </template>
 <script lang="ts" setup>
 import Card from './Card.vue'
+import { ref, reactive } from 'vue'
+
+const input = reactive({
+  items: [
+    { key: 'a.b.c', value: '1' },
+    { key: 'a.b.d', value: '2' },
+    { key: 'a.e.f', value: '3' }
+  ],
+  output: {}
+});
+
+function addItem() {
+  input.items.push({ key: '', value: '' });
+}
+
+function generateObject() {
+  input.output = {};
+
+  input.items.forEach((item:any) => {
+    if (item.key && item.value) {
+      let objRef = input.output as any;
+      const keys = item.key.split('.');
+      keys.forEach((key:any, index:any) => {
+        if (index === keys.length - 1) {
+          objRef[key] = item.value;
+        } else {
+          if (!objRef.hasOwnProperty(key)) {
+            objRef[key] = {};
+          }
+          objRef = objRef[key];
+        }
+      });
+    }
+  });
+}
+
 </script>
--- 02/src/views/Home.vue
+++ 02/src/views/Home.vue
@@ -12,19 +12,3 @@
 import LeftCard from '~/components/LeftCard/index.vue'
 import RightCard from '~/components/RightCard/index.vue'
 </script>
-
-
-<style scoped>
-.logo {
-  height: 6em;
-  padding: 1.5em;
-  will-change: filter;
-  transition: filter 300ms;
-}
-.logo:hover {
-  filter: drop-shadow(0 0 2em #646cffaa);
-}
-.logo.vue:hover {
-  filter: drop-shadow(0 0 2em #42b883aa);
-}
-</style>
